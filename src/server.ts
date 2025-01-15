4cf1347f239ba05a21ab6c04c28ffe41282dd38a
JarolyP <jobs13az@gmail.com>
2025-01-14 22:43:22 -0400

    Creando un schema para la base de datos

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import 'dotenv/config';
 import router from './router';
-import {connectDB} from './db';
+import {connectDB} from './config/db';
 
 const app = express();
 
